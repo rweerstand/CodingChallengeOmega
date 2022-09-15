2debfcc333c37ecae3245445097c964898b7430f
ps-kr <113337447+ps-kr@users.noreply.github.com>
2022-09-15 09:47:48 +0200

    Create opdracht1.c

---
+++ opdracht1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    for (i = 0; i < 5; i++) {
+        if (i % 2) {
+            printf("Doei Kruitbosch\n");
+        } else {
+            printf("Hallo Kruitbosch\n");
+        }
+    }
+    return 0;
+}
