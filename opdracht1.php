6c4930430f133fcf518911d1963da1c6e2a2400d
bwolken1 <113582478+bwolken1@users.noreply.github.com>
2022-09-15 13:04:13 +0200

    Create opdracht1.php

---
+++ opdracht1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php  
+$x = 0;
+$s = 0; 
+while($x <= 2 && $x == $s) {
+  echo "Hallo Kruitbosch <br>";
+  $x+=1;
+  while($s <= 1 && $x != $s) {
+    echo "Doei Kruitbosch <br>";
+    $s+=1;
+  }
+}
+?>  
+
+</body>
+</html>
